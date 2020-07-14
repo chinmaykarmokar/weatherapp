3cc56f06a0956cd1f2ed88f0ddc3ad58bab0b646
chinmaykarmokar <chinmaykarmokar@gmail.com>
2020-07-14 12:10:42 +0530

    Updates

---
+++ precache-manifest.6affc8a70cec6fb3a4854df48a3adf64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2756477b5e87a21821e7a7da575ad3c1",
+    "url": "/chinmaykarmokar/weatherapp.git/index.html"
+  },
+  {
+    "revision": "4616aa5651ffabee3f96",
+    "url": "/chinmaykarmokar/weatherapp.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "422e4e190a9a6be2a542",
+    "url": "/chinmaykarmokar/weatherapp.git/static/css/main.add9bbe1.chunk.css"
+  },
+  {
+    "revision": "4616aa5651ffabee3f96",
+    "url": "/chinmaykarmokar/weatherapp.git/static/js/2.838a5772.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/chinmaykarmokar/weatherapp.git/static/js/2.838a5772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422e4e190a9a6be2a542",
+    "url": "/chinmaykarmokar/weatherapp.git/static/js/main.ad7412fe.chunk.js"
+  },
+  {
+    "revision": "baf767f71b90de60faaf",
+    "url": "/chinmaykarmokar/weatherapp.git/static/js/runtime-main.dc899266.js"
+  },
+  {
+    "revision": "5f600f2f7ab98ba939f720034e791b50",
+    "url": "/chinmaykarmokar/weatherapp.git/static/media/nature.5f600f2f.jpg"
+  }
+]);
